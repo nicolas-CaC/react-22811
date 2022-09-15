79bf98efcf3eb723e7b327e088c1f078f7fc2081
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2022-09-14 21:37:39 -0300

    clase 8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Menu } from './Menu';
 
-function App() {
+const App = () => {
+
+  const container = 'flex flex-col items-center';
+  const hello = "text-3xl font-bold underline";
+  const holis = 'text-5xl w-fit bg-red-800 text-neutral-100 px-4 rounded';
+  const buttonCss = ' bg-blue-400 text-white px-4 py-2 rounded-full hover:bg-blue-800 transition duration-300'
+
+  const numero = 0;
+
+  function clickHandler() {
+    console.log(numero)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ container }>
+      <div className={ holis }>Holis</div>
+      <h1 className={ hello }>
+        Hello world!
+      </h1>
+      <Menu nuevaOpcion='Productos' />
+      <button
+        className={ buttonCss }
+        onClick={ clickHandler }
+      >Soy un boton
+      </button>
     </div>
   );
 }
